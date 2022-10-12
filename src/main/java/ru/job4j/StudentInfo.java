87062c2af584d8ca1668b9acc2c7eff2c54c3104
solov <soloviov.alexandr2012@yandex.ru>
2022-10-12 11:19:13 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Alexandr Soloviov");
+        System.out.println("14.09.1978");
+    }
+}
