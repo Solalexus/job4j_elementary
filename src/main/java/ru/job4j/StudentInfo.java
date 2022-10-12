5a854d6b2833cc274c0420699bfe6f8790c6625f
solov <soloviov.alexandr2012@yandex.ru>
2022-10-12 11:31:45 +0300

    3.2. Фиксация изменений на github [#243562]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Alexandr Soloviov");
         System.out.println("14.09.1978");
     }
