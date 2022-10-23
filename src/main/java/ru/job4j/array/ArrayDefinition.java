1a388fa743d228ef0e81d3db691d5a8416d50c26
solov <soloviov.alexandr2012@yandex.ru>
2022-10-23 19:00:38 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Ivan Krylov";
+        names[1] = "Dmitriy Puchkov";
+        names[2] = "Sergey Novikov";
+        names[3] = "Pavel Ivanov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
